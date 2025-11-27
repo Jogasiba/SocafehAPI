c577d588d71a1d7277c8a1b71fa46ada5d58f7cb
João Gabriel Simon Batista <135073975+Jogasiba@users.noreply.github.com>
2025-11-27 18:54:41 -0300

    trocando nome do banco

--- banco.js
+++ banco.js
@@ -1,5 +1,5 @@
 import { Sequelize, DataTypes } from 'sequelize';
-const sequelize = new Sequelize('Banco_Cafeteria', 'postgres', 'Socafeh123', {
+const sequelize = new Sequelize('bancoSocafeh', 'postgres', 'Socafeh123', {
     host: 'database-1.cvmu0ukcqmtc.us-east-2.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
@@ -11,4 +11,5 @@ const sequelize = new Sequelize('Banco_Cafeteria', 'postgres', 'Socafeh123', {
 
 
 export default sequelize;
+
 
