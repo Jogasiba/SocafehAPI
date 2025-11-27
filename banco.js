035d235f182d71b457a3a624b0d4148fdad5b12a
João Gabriel Simon Batista <135073975+Jogasiba@users.noreply.github.com>
2025-11-27 18:50:18 -0300

    atualizando senha
    Updated database password for PostgreSQL connection.

--- banco.js
+++ banco.js
@@ -1,5 +1,5 @@
 import { Sequelize, DataTypes } from 'sequelize';
-const sequelize = new Sequelize('Banco_Cafeteria', 'postgres', 'postgres', {
+const sequelize = new Sequelize('Banco_Cafeteria', 'postgres', 'Socafeh123', {
     host: 'database-1.cvmu0ukcqmtc.us-east-2.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
@@ -11,3 +11,4 @@ const sequelize = new Sequelize('Banco_Cafeteria', 'postgres', 'postgres', {
 
 
 export default sequelize;
+
