a0aa53587161ca7adc3203517fb891b47920a2e2
João Gabriel Simon Batista <135073975+Jogasiba@users.noreply.github.com>
2025-11-28 23:12:11 -0300

    ajustando de uma vez por todas

--- banco.js
+++ banco.js
@@ -1,5 +1,5 @@
 import { Sequelize, DataTypes } from 'sequelize';
-const sequelize = new Sequelize('rdsadmin', 'postgres', 'Socafeh123', {
+const sequelize = new Sequelize('bancoSocafeh', 'postgres', 'Socafeh123', {
     host: 'database-1.cvmu0ukcqmtc.us-east-2.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
