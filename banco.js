f733766d20148d6b69651b3ec10fdcd45f37fc04
João Gabriel Simon Batista <135073975+Jogasiba@users.noreply.github.com>
2025-11-28 23:12:39 -0300

    ajustando de vrdd agr

--- banco.js
+++ banco.js
@@ -1,13 +1,17 @@
 import { Sequelize, DataTypes } from 'sequelize';
+
 const sequelize = new Sequelize('bancoSocafeh', 'postgres', 'Socafeh123', {
     host: 'database-1.cvmu0ukcqmtc.us-east-2.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // evita erro de certificado
+        }
+    },
     define: {
         timestamps: false,
         freezeTableName: true,
     }
 });
-
-
-export default sequelize;
